343e87f5c9c39b5db40e7c0991d729a0d6c18a2d
Paul-Maxime <github@paulmaxime.fr>
2022-10-01 22:29:57 +0200

    Timer fix

--- src/components/RemainingTime.vue
+++ src/components/RemainingTime.vue
@@ -1,6 +1,6 @@
 <script setup>
-import game from "@/stores/game.js";
 import { ref, watch } from "vue";
+import game from "@/stores/game.js";
 
 const MOON_PHASES = ["🌕", "🌖", "🌗", "🌘", "🌑"];
 
@@ -9,6 +9,7 @@ const moon = ref("");
 
 watch(game, () => {
   timer.value = (game.remainingTime / 10).toFixed(1);
+  if (timer.value === "10.0") timer.value = "9.9";
   const currentPhase = Math.floor(MOON_PHASES.length - 1 - game.remainingTime / (100 / (MOON_PHASES.length - 1)));
   moon.value = MOON_PHASES[currentPhase];
 });
@@ -16,7 +17,7 @@ watch(game, () => {
 
 <template>
   <div class="timer">
-    <span class="yooo" v-emoji="moon"></span>
+    <span v-emoji="moon"></span>
     <small>{{ timer }}</small>
   </div>
 </template>
