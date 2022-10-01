ab7e44f968e71e896afaf5997a2bd08480480cff
Jean Fauquenot <jean.fauquenot@gmail.com>
2022-10-01 14:16:30 +0200

    Add GameLevel.vue and play with the css

---
+++ src/components/GameLevel.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      count: twemoji.parse("⚔️🐔"),
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="game-level">
+    <div v-html="count"></div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+.game-level {
+  top: 0;
+  left: 0;
+  padding: 10px;
+  flex: 0 0 100%;
+  border: 1px dashed black;
+  flex-wrap: wrap;
+  background-color: lightcoral;
+}
+</style>
