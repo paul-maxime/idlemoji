f71671ef0238a5e7c35ba875206ded0017af82dc
Paul-Maxime <github@paulmaxime.fr>
2022-10-01 17:36:08 +0200

    Add the timer and gameover

--- src/stores/game.js
+++ src/stores/game.js
@@ -1,6 +1,14 @@
 import { reactive } from "vue";
 
 const game = reactive({
+  remainingTime: 100,
+  isGameOver: false,
+  respawnTimer: 10,
+  player: {
+    exp: 100,
+    speed: 1,
+    position: 0,
+  },
   entities: [
     { type: "⚔", position: 0 },
     { type: "🐔", position: 5 },
@@ -14,7 +22,25 @@ const game = reactive({
     { name: "More damage", cost: 200, level: 2 },
   ],
   tick() {
-    this.exp += 1;
+    if (this.isGameOver) {
+      this.respawnTimer -= 1;
+      if (this.respawnTimer <= 0) {
+        this.respawn();
+      }
+      return;
+    }
+    this.remainingTime -= 1;
+    if (this.remainingTime <= 0) {
+      this.gameOver();
+    }
+  },
+  gameOver() {
+    this.isGameOver = true;
+    this.respawnTimer = 10;
+  },
+  respawn() {
+    this.isGameOver = false;
+    this.remainingTime = 100;
   },
 });
 
