55c6cb4582b40a1d1c014221bb08a1d1b4870884
Paul-Maxime <github@paulmaxime.fr>
2022-10-02 14:39:07 +0200

    Hide the golds until unlocked

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -3,6 +3,10 @@ import game from "@/stores/game.js";
 import { watch } from "vue";
 
 watch(game, () => {
-  document.title = `${game.gold} gold • run ${game.currentRun} • lemojid`;
+  if (game.unlocks.has("gold-display")) {
+    document.title = `${game.gold} gold • run ${game.currentRun} • lemojid`;
+  } else {
+    document.title = `run ${game.currentRun} • lemojid`;
+  }
 });
 </script>
