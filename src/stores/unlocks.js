bd51da558adb17b8f363ecb1febe40b15863e443
Jean Fauquenot <jean.fauquenot@gmail.com>
2022-10-03 23:57:04 +0200

    Increase market cost of main upgrade

--- src/stores/unlocks.js
+++ src/stores/unlocks.js
@@ -4,9 +4,9 @@ const FEATURES = [
   { id: "gold-display", text: "Unlock the current gold", cost: 5 },
   { id: "level-gui", text: "Unlock the graphical interface", cost: 10 },
   { id: "hero-stats", text: "Unlock the hero upgrades", cost: 10 },
-  { id: "remaining-time", text: "Unlock the remaining time", cost: 20 },
-  { id: "hero-stats-details", text: "Unlock the upgrade details", cost: 30 },
-  { id: "battle-info", text: "Unlock the battle information", cost: 50 },
+  { id: "remaining-time", text: "Unlock the remaining time", cost: 100 },
+  { id: "hero-stats-details", text: "Unlock the upgrade details", cost: 200 },
+  { id: "battle-info", text: "Unlock the battle information", cost: 500 },
 ];
 
 const unlocked = reactive({
